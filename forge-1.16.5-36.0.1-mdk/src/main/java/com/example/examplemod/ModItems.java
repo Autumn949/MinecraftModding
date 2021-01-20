74ae3d76244b8285f4b6563f0ab76989773e3ab1
Autumn <epicawsomealex@gmail.com>
2021-01-19 21:14:45 -0500

    BackPack init
    inital add of backpack still has mainly flower code

--- forge-1.16.5-36.0.1-mdk/src/main/java/com/example/examplemod/ModItems.java
+++ forge-1.16.5-36.0.1-mdk/src/main/java/com/example/examplemod/ModItems.java
@@ -1,6 +1,7 @@
 package com.example.examplemod;
 
 import com.example.examplemod.Registration;
+import com.example.examplemod.items.TutorialBackpack;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.inventory.EquipmentSlotType;
@@ -13,7 +14,7 @@ public class ModItems {
             new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
     //ArmorItem
     public static final RegistryObject<Item> TUTORIAL_HELMET = Registration.ITEMS.register("tutorial_helmet",() -> new ArmorItem(TutorialArmorMaterial.TUTORIAL_ARMOR_MATERIAL, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)));
-
+    public static final RegistryObject<Item> TUTORIAL_BACKPACK = Registration.ITEMS.register("tutorial_backpack", ()-> new TutorialBackpack(TutorialArmorMaterial.TUTORIAL_ARMOR_MATERIAL,EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.TOOLS)));
     //ToolItem
     public static final RegistryObject<Item> TUTORIAL_AXE = Registration.ITEMS.register("tutorial_axe",() -> new AxeItem(TutorialToolMaterial.TUTORIAL_TOOL_MATERIAL,9, 2, new Item.Properties().group(ItemGroup.COMBAT)));
     static void register() {}
